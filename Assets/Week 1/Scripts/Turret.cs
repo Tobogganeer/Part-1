66d09fbbef693437b1b522b1524240c028d94f44
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-01-10 14:11:15 -0500

    Task 4a improvement: clamp barrel rotation to avoid becoming a helicopter

--- Assets/Week 1/Scripts/Turret.cs
+++ Assets/Week 1/Scripts/Turret.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     public float rotationSpeed = 180f;
+    public float minimumAngle = -5f;
+    public float maximumAngle = 70f;
 
     void Update()
     {
         float rotationInput = Input.GetAxis("Vertical");
-        transform.Rotate(Vector3.forward, rotationInput * rotationSpeed * Time.deltaTime);
+        float barrelAngle = transform.eulerAngles.z;
+        if ((barrelAngle > minimumAngle && rotationInput < 0) || (barrelAngle < maximumAngle && rotationInput > 0))
+        {
+            transform.Rotate(Vector3.forward, rotationInput * rotationSpeed * Time.deltaTime);
+        }
     }
 }
