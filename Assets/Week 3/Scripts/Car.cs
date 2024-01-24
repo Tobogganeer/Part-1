f0a6ba3ccdfd0f02d38bcda53c10cb017742640d
Evan Daveikis <evandaveikis@gmail.com>
2024-01-24 14:14:16 -0500

    Task 3 complete: car controller

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float steeringSpeed = 300f;
+    public float forwardSpeed = 1000f;
+    public float maxSpeed = 100f;
+
+    Vector2 input;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        float torque = steeringSpeed * -input.x * Time.deltaTime;
+        // Only rotate if we are trying to move forwards or backwards
+        // This also corrects our backwards steering
+        rb.AddTorque(torque * input.y);
+
+        if (rb.velocity.sqrMagnitude < (maxSpeed * maxSpeed))
+        {
+            // Do float operations first for better performance (humungous 0.02us savings)
+            Vector3 force = transform.up * (input.y * forwardSpeed * Time.deltaTime);
+            rb.AddForce(force);
+        }
+    }
+}
