8afc841b4647b0e6d301d5a98ac34192a1524353
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-01-28 20:29:29 -0500

    BlackHoleObject class created

---
+++ Assets/Assignment/Scripts/BlackHoleObject.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BlackHoleObject : MonoBehaviour
+{
+    // A list of all BlackHoleObjects
+    public static List<BlackHoleObject> All { get; private set; } = new List<BlackHoleObject>();
+    // Called when this thingy enters the black hole
+    public Action<GameObject> OnEnterBlackHole { get; private set; }
+
+    // Not assigned, but needs to be accessed
+    [HideInInspector]
+    public Rigidbody2D rb { get; private set; }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        All.Add(this);
+    }
+
+    public void EnteredBlackHole()
+    {
+        // Uh oh, time to take appropriate action...
+        OnEnterBlackHole?.Invoke(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        All.Remove(this);
+    }
+}
+
+/*
+
+BlackHoleObject.cs:
+- Pulled towards the black hole
+- Calls a function when entering the black hole
+- Pseudocode
+  - Variable for rb, callback
+  - Static list of BlackHoleObjects
+  - Add self to list on start
+  - Remove self from list on destroy
+  - Action<> OnEnterBlackHole called by BlackHole.cs
+
+*/
